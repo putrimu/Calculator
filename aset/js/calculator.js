41726b9e491a9bfceab9a25c2d9e4de3987ad5f3
putrimu <putrimariahulfa@gmail.com>
2018-12-06 10:10:31 +0700

    adding aset

---
+++ aset/js/calculator.js
@@ -0,0 +1,31 @@
+function tambah() {
+    val = document.getElementsByName('nilai')[0].value;
+    document.getElementsByName('nilai')[0].value += "+";
+}
+
+function kurang() {
+    val = document.getElementsByName('nilai')[0].value;
+    document.getElementsByName('nilai')[0].value += "-";
+}
+
+function kali() {
+    val = document.getElementsByName('nilai')[0].value;
+    document.getElementsByName('nilai')[0].value += "*";
+}
+
+function bagi() {
+    val = document.getElementsByName('nilai')[0].value;
+    document.getElementsByName('nilai')[0].value += "/";
+}
+
+function hitung() {
+    val = document.getElementsByName('nilai')[0].value;
+    document.getElementsByName('hasil')[0].value = eval(val);
+}
+
+function runC() {
+    val = document.getElementsByName('nilai')[0].value;
+    document.getElementsByName('nilai')[0].value = "";
+    val = document.getElementsByName('hasil')[0].value;
+    document.getElementsByName('hasil')[0].value = "";
+};
